c2ffea812c4a473499bf21f2ca72a9038ef8d1cc
Ikbol <muxtorovikboljon1@gmail.com>
2024-08-23 23:29:16 +0500

    yana

--- shop/shop/wsgi.py
+++ shop/shop/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShop.shop.shop.settings')
 application = get_wsgi_application()
 
