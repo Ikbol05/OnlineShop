8c42c42ab7ab9d1983c45bcdd72cb3024c656dac
Ikbol <muxtorovikboljon1@gmail.com>
2024-08-24 01:14:59 +0500

    dvger

--- shop/shop/wsgi.py
+++ shop/shop/wsgi.py
@@ -11,17 +11,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShop.settings')
-# application = get_wsgi_application()
-
-path = '/home/Ikboljon/OnlineShop/shop/blog'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'OnlineShop.settings'
-
-# then:
-
-from django.core.wsgi import get_wsgi_application
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShop.settings')
 application = get_wsgi_application()
+
+
