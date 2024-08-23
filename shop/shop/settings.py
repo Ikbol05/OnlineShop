ecf089331f027432e364c3c178b9dddd552d4bc5
Ikbol <muxtorovikboljon1@gmail.com>
2024-08-24 01:42:58 +0500

    ferferf

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-baab8$2)f#(xy1uu4tt5#eh#hnq@ie^xc8-^av3bwxwe1a8tqh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
